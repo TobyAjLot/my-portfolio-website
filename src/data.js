04210ad2a3557c881295c47cf4496cd2e1a6d02b
tobyajlot <tobyajlot@gmail.com>
2023-08-11 16:01:33 +0100

    Update Components

--- src/data.js
+++ src/data.js
@@ -23,50 +23,6 @@ export const skills = [
   "Github",
 ];
 
-export const projects = [
-  {
-    title: "Tobi's To-Do Lists",
-    description:
-      "This is an app that lets you add pending tasks you need to complete at a later time. You can also create a custom list by adding /(name of list) to the home page.",
-    image: "todo-list",
-    link: "https://reactbootcamp.com",
-  },
-  {
-    title: "Secrets App",
-    description:
-      "Share that secret that is bugging your mind. Don't worry, No one will know its you.",
-    image: "secrets",
-    link: "https://reedbarger.com",
-  },
-  {
-    title: "Tobi's Blog",
-    description: "Stay up to date with the fast changing tech space.",
-    image: "blog",
-    link: "https://jsbootcamp.com",
-  },
-  {
-    title: "Dice Game",
-    description:
-      "Challenge your friends to the epic dice rolling and see your score go up the board. May the best friend win!",
-    image: "dice-game",
-    link: "https://pythonbootcamp.com",
-  },
-  {
-    title: "Simon Game",
-    description:
-      "This is a version of the Simon electronic game of short-term memory skill invented by Ralph H. Baer and Howard J. How far can you go?",
-    image: "simon-game",
-    link: "https://pythonbootcamp.com",
-  },
-  {
-    title: "Drum-Kit",
-    description:
-      "Wanna have some fun with the sound of the Drumset? Try the autoplay.",
-    image: "drum-kit",
-    link: "https://pythonbootcamp.com",
-  },
-];
-
 export const contact = [
   {
     title: "Email",
