6862cdb96ec723f074e0da1868cd83e3b541f5e9
tobyajlot <tobyajlot@gmail.com>
2023-08-11 15:37:59 +0100

    First Deply Commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,10 +16,14 @@ const Projects = () => {
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
             Apps I've Built
           </h1>
-          <div className="w-full flex flex-wrap px-2 my-4">
-            <div className="relative w-full lg:w-1/2 p-2 flex items-center">
-              <img src={Todo} alt="gallery" className="rounded-lg" />
-              <div className="px-8 py-2 absolute z-10 w-full bg-gray-900 opacity-0 duration-300 hover:opacity-100 rounded-lg inset-0">
+          <div className="flex flex-wrap m-8">
+            <div className="sm:w-1/2 w-100 p-4 flex relative">
+              <img
+                src={Todo}
+                alt="gallery"
+                className="rounded-3xl p-4 absolute inset-0 w-full h-full object-cover object-center"
+              />
+              <div className="px-8 py-2 relative z-10 w-full bg-gray-900 opacity-0 duration-300 hover:opacity-100 rounded-lg inset-0">
                 <h1 className="title-font text-3xl font-medium text-white mb-3">
                   Tobi's To-Do List
                 </h1>
@@ -38,22 +42,31 @@ const Projects = () => {
                     <li> Lodash</li>
                   </ul>
                 </div>
-                {/* <div className="">
+                <div className="mt-2 pb-4">
                   <p>
                     <span className="font-bold">Descripton: </span>This app lets
                     you add pending tasks you need to complete at a later time.
                     You can also create a custom list by adding /(name of list)
                     to the home page
                   </p>
-                </div> */}
+                </div>
                 <button className="relative text-black bg-gray-200 p-2 rounded">
-                  Check it out
+                  <a
+                    href="https://todolist-toby-app.onrender.com"
+                    target="_blank"
+                  >
+                    Check it out
+                  </a>
                 </button>
               </div>
             </div>
-            <div className="relative w-full lg:w-1/2 p-2 flex items-center">
-              <img src={Secrets} alt="gallery" className="rounded-lg" />
-              <div className="px-8 py-10 absolute z-10 w-full bg-gray-900 opacity-0 duration-300 hover:opacity-100 rounded inset-0">
+            {/* <div className="sm:w-1/2 w-100 p-4 flex relative">
+              <img
+                src={Secrets}
+                alt="gallery"
+                className="rounded-3xl p-4 absolute inset-0 w-full h-full object-cover object-center"
+              />
+              <div className="px-8 py-2 relative z-10 w-full bg-gray-900 opacity-0 duration-300 hover:opacity-100 rounded-lg inset-0">
                 <h1 className="title-font text-3xl font-medium text-white mb-3">
                   Secrets App
                 </h1>
@@ -65,14 +78,14 @@ const Projects = () => {
                     <li> Node JS</li>
                     <li> Mongoose </li>
                   </ul>
-                  <ul className="technologies mb-8">
+                  <ul className="technologies mb-2">
                     <li> Express JS</li>
                     <li> MD5 </li>
                     <li> MongoDB</li>
                     <li> Google-OAuth2 </li>
                   </ul>
                 </div>
-                <div>
+                <div className="mt-2 pb-4">
                   <p>
                     <span className="font-bold">Descripton: </span>Share that
                     secret that is bugging your mind. Don't worry, No one will
@@ -83,10 +96,14 @@ const Projects = () => {
                   Check it out
                 </button>
               </div>
-            </div>
-            <div className="relative w-full lg:w-1/2 p-2 flex items-center">
-              <img src={Blog} alt="gallery" className="rounded-lg" />
-              <div className="px-8 py-10 absolute z-10 w-full bg-gray-900 opacity-0 duration-300 hover:opacity-100 rounded inset-0">
+            </div> */}
+            {/* <div className="sm:w-1/2 w-100 p-4 flex relative">
+              <img
+                src={Blog}
+                alt="gallery"
+                className="rounded-3xl p-4 absolute inset-0 w-full h-full object-cover object-center"
+              />
+              <div className="px-8 py-2 relative z-10 w-full bg-gray-900 opacity-0 duration-300 hover:opacity-100 rounded-lg inset-0">
                 <h1 className="title-font text-3xl font-medium text-white mb-3">
                   Tobi's Blog
                 </h1>
@@ -97,14 +114,14 @@ const Projects = () => {
                     <li>Bootstrap</li>
                     <li> MongoDB</li>
                   </ul>
-                  <ul className="technologies mb-8">
+                  <ul className="technologies mb-2">
                     <li> Node JS</li>
                     <li> Express JS</li>
                     <li> EJS </li>
                     <li> Mongoose </li>
                   </ul>
                 </div>
-                <div>
+                <div className="mt-2 pb-4">
                   <p>
                     <span className="font-bold">Descripton: </span>Stay up to
                     date with the fast changing tech space
@@ -114,10 +131,14 @@ const Projects = () => {
                   Check it out
                 </button>
               </div>
-            </div>
-            <div className="relative w-full lg:w-1/2 p-2 flex items-center">
-              <img src={Dicegame} alt="gallery" className="rounded-lg" />
-              <div className="px-8 py-10 absolute z-10 w-full bg-gray-900 opacity-0 duration-300 hover:opacity-100 rounded inset-0">
+            </div> */}
+            <div className="sm:w-1/2 w-100 p-4 flex relative">
+              <img
+                src={Dicegame}
+                alt="gallery"
+                className="rounded-3xl p-4 absolute inset-0 w-full h-full object-cover object-center"
+              />
+              <div className="px-8 py-2 relative z-10 w-full bg-gray-900 opacity-0 duration-300 hover:opacity-100 rounded-lg inset-0">
                 <h1 className="title-font text-3xl font-medium text-white mb-3">
                   Dice Game
                 </h1>
@@ -129,7 +150,7 @@ const Projects = () => {
                     <li> Javascript</li>
                   </ul>
                 </div>
-                <div>
+                <div className="mt-2 pb-4">
                   <p>
                     <span className="font-bold">Descripton: </span>Challenge
                     your friends to the epic dice rolling and see your score go
@@ -137,13 +158,22 @@ const Projects = () => {
                   </p>
                 </div>
                 <button className="relative text-black bg-gray-200 p-2 rounded">
-                  Check it out
+                  <a
+                    href="https://tobyajlot.github.io/Dice-game"
+                    target="_blank"
+                  >
+                    Check it out
+                  </a>
                 </button>
               </div>
             </div>
-            <div className="relative w-full lg:w-1/2 p-2 flex items-center">
-              <img src={Simongame} alt="gallery" className="rounded-lg" />
-              <div className="px-8 py-10 absolute z-10 w-full bg-gray-900 opacity-0 duration-300 hover:opacity-100 rounded inset-0">
+            <div className="sm:w-1/2 w-100 p-4 flex relative">
+              <img
+                src={Simongame}
+                alt="gallery"
+                className="rounded-3xl p-4 absolute inset-0 w-full h-full object-cover object-center"
+              />
+              <div className="px-8 py-2 relative z-10 w-full bg-gray-900 opacity-0 duration-300 hover:opacity-100 rounded-lg inset-0">
                 <h1 className="title-font text-3xl font-medium text-white mb-3">
                   Simon Game
                 </h1>
@@ -155,7 +185,7 @@ const Projects = () => {
                     <li> Javascript</li>
                   </ul>
                 </div>
-                <div>
+                <div className="mt-2 pb-4">
                   <p>
                     <span className="font-bold">Descripton: </span>This is a
                     version of the Simon electronic game of short-term memory
@@ -164,13 +194,22 @@ const Projects = () => {
                   </p>
                 </div>
                 <button className="relative text-black bg-gray-200 p-2 rounded">
-                  Check it out
+                  <a
+                    href="https://tobyajlot.github.io/Simon-Game"
+                    target="_blank"
+                  >
+                    Check it out
+                  </a>
                 </button>
               </div>
             </div>
-            <div className="relative w-full lg:w-1/2 p-2 flex items-center">
-              <img src={Drumkit} alt="gallery" className="rounded-lg" />
-              <div className="px-8 py-10 absolute z-10 w-full bg-gray-900 opacity-0 duration-300 hover:opacity-100 rounded inset-0">
+            <div className="sm:w-1/2 w-100 p-4 flex relative">
+              <img
+                src={Drumkit}
+                alt="gallery"
+                className="rounded-3xl p-4 absolute inset-0 w-full h-full object-cover object-center"
+              />
+              <div className="px-8 py-2 relative z-10 w-full bg-gray-900 opacity-0 duration-300 hover:opacity-100 rounded-lg inset-0">
                 <h1 className="title-font text-3xl font-medium text-white mb-3">
                   Drumkit
                 </h1>
@@ -182,14 +221,19 @@ const Projects = () => {
                     <li> Javascript</li>
                   </ul>
                 </div>
-                <div>
+                <div className="mt-2 pb-4">
                   <p>
                     <span className="font-bold">Descripton: </span>Wanna have
                     some fun with the sound of the Drumset? Try the autoplay
                   </p>
                 </div>
                 <button className="relative text-black bg-white p-2 rounded">
-                  Check it out
+                  <a
+                    href="https://tobyajlot.github.io/Drum-Kit"
+                    target="_blank"
+                  >
+                    Check it out
+                  </a>
                 </button>
               </div>
             </div>
